5b714a4f0c8a95b958f41d6c249da4bb12f653c5
Daz Jones <yuki@thebronasium.com>
2012-06-29 20:02:18 +0100

    Update

--- u8818-vendor-blobs.mk
+++ u8818-vendor-blobs.mk
@@ -91,23 +91,25 @@ PRODUCT_COPY_FILES += \
     vendor/huawei/u8818/proprietary/lib/libuim.so:system/lib/libuim.so \
     vendor/huawei/u8818/proprietary/lib/libping_mdm.so:system/lib/libping_mdm.so \
     vendor/huawei/u8818/proprietary/lib/liboemcamera.so:system/lib/liboemcamera.so \
-#    vendor/huawei/u8818/proprietary/bin/mm-qcamera-daemon:system/bin/mm-qcamera-daemon \
-#    vendor/huawei/u8818/proprietary/bin/mm-qcamera-test:system/bin/mm-qcamera-test \
-#    vendor/huawei/u8818/proprietary/bin/mm-qcamera-testsuite-client:system/bin/mm-qcamera-testsuite-client \
-#    vendor/huawei/u8818/proprietary/bin/v4l2-qcamera-app:system/bin/v4l2-qcamera-app \
-#    vendor/huawei/u8818/proprietary/lib/liboemcamera.so:system/lib/liboemcamera.so \
-#    vendor/huawei/u8818/proprietary/lib/liboemcamera.so:obj/lib/liboemcamera.so \
-#    vendor/huawei/u8818/proprietary/lib/libcamera.so:obj/lib/libcamera.so \
-#    vendor/huawei/u8818/proprietary/lib/libcamera.so:system/lib/libcamera.so \
-#    vendor/huawei/u8818/proprietary/lib/libmmjpeg.so:system/lib/libmmjpeg.so \
-#    vendor/huawei/u8818/proprietary/lib/libmmipl.so:system/lib/libmmipl.so \
-#    vendor/huawei/u8818/proprietary/lib/libchromatix_mt9e013_ar.so:system/lib/libchromatix_mt9e013_ar.so \
-#    vendor/huawei/u8818/proprietary/lib/libchromatix_mt9e013_default_video.so:system/lib/libchromatix_mt9e013_default_video.so \
-#    vendor/huawei/u8818/proprietary/lib/libchromatix_mt9e013_preview.so:system/lib/libchromatix_mt9e013_preview.so \
-#    vendor/huawei/u8818/proprietary/lib/libchromatix_mt9e013_video_hfr.so:system/lib/libchromatix_mt9e013_video_hfr.so \
-#    vendor/huawei/u8818/proprietary/lib/libchromatix_mt9p017_ar.so:system/lib/libchromatix_mt9p017_ar.so \
-#    vendor/huawei/u8818/proprietary/lib/libchromatix_mt9p017_preview.so:system/lib/libchromatix_mt9p017_preview.so \
-#    vendor/huawei/u8818/proprietary/lib/libchromatix_mt9p017_video.so:system/lib/libchromatix_mt9p017_video.so \
-#    vendor/huawei/u8818/proprietary/lib/libchromatix_s5k4e1_ar.so:system/lib/libchromatix_s5k4e1_ar.so \
-#    vendor/huawei/u8818/proprietary/lib/libchromatix_s5k4e1_default_video.so:system/lib/libchromatix_s5k4e1_default_video.so \
-#    vendor/huawei/u8818/proprietary/lib/libchromatix_s5k4e1_preview.so:system/lib/libchromatix_s5k4e1_preview.so \
+	vendor/huawei/u8818/proprietary/lib/liboemcamera.so:obj/lib/liboemcamera.so \
+    vendor/huawei/u8818/proprietary/bin/mm-qcamera-daemon:system/bin/mm-qcamera-daemon \
+    vendor/huawei/u8818/proprietary/bin/mm-qcamera-test:system/bin/mm-qcamera-test \
+    vendor/huawei/u8818/proprietary/bin/mm-qcamera-testsuite-client:system/bin/mm-qcamera-testsuite-client \
+    vendor/huawei/u8818/proprietary/bin/test_gemini:system/bin/test_gemini \
+    vendor/huawei/u8818/proprietary/bin/v4l2-qcamera-app:system/bin/v4l2-qcamera-app \
+    vendor/huawei/u8818/proprietary/lib/libcamera.so:obj/lib/libcamera.so \
+    vendor/huawei/u8818/proprietary/lib/libcamera.so:system/lib/libcamera.so \
+    vendor/huawei/u8818/proprietary/lib/hw/camera.msm7x27.so:system/lib/hw/camera.msm7x27.so \
+    vendor/huawei/u8818/camera/lib/libgemini.so:system/lib/libgemini.so \
+    vendor/huawei/u8818/proprietary/lib/libmmjpeg.so:system/lib/libmmjpeg.so \
+    vendor/huawei/u8818/proprietary/lib/libmmipl.so:system/lib/libmmipl.so \
+    vendor/huawei/u8818/proprietary/lib/libchromatix_mt9e013_ar.so:system/lib/libchromatix_mt9e013_ar.so \
+    vendor/huawei/u8818/proprietary/lib/libchromatix_mt9e013_default_video.so:system/lib/libchromatix_mt9e013_default_video.so \
+    vendor/huawei/u8818/proprietary/lib/libchromatix_mt9e013_preview.so:system/lib/libchromatix_mt9e013_preview.so \
+    vendor/huawei/u8818/proprietary/lib/libchromatix_mt9e013_video_hfr.so:system/lib/libchromatix_mt9e013_video_hfr.so \
+    vendor/huawei/u8818/proprietary/lib/libchromatix_mt9p017_ar.so:system/lib/libchromatix_mt9p017_ar.so \
+    vendor/huawei/u8818/proprietary/lib/libchromatix_mt9p017_preview.so:system/lib/libchromatix_mt9p017_preview.so \
+    vendor/huawei/u8818/proprietary/lib/libchromatix_mt9p017_video.so:system/lib/libchromatix_mt9p017_video.so \
+    vendor/huawei/u8818/proprietary/lib/libchromatix_s5k4e1_ar.so:system/lib/libchromatix_s5k4e1_ar.so \
+    vendor/huawei/u8818/proprietary/lib/libchromatix_s5k4e1_default_video.so:system/lib/libchromatix_s5k4e1_default_video.so \
+    vendor/huawei/u8818/proprietary/lib/libchromatix_s5k4e1_preview.so:system/lib/libchromatix_s5k4e1_preview.so
