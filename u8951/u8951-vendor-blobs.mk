deb63fd2a19b65dbf41d0cebf9518a7f79e5d166
Y300-0100 <bosko.brajovich@gmail.com>
2014-10-06 10:48:22 +0200

    nfcee is build

--- u8951/u8951-vendor-blobs.mk
+++ u8951/u8951-vendor-blobs.mk
@@ -17,7 +17,6 @@
 PRODUCT_COPY_FILES += \
     vendor/huawei/u8951/proprietary/bin/nfc_check:system/bin/nfc_check \
     vendor/huawei/u8951/proprietary/etc/EEDATA.cfg:system/etc/EEDATA.cfg \
-    vendor/huawei/u8951/proprietary/etc/nfcee_access.xml:system/etc/nfcee_access.xml \
     vendor/huawei/u8951/proprietary/lib/hw/nfc.huawei.so:system/lib/hw/nfc.huawei.so \
     vendor/huawei/u8951/proprietary/lib/libifdsmartcardapi.so:system/lib/libifdsmartcardapi.so \
     vendor/huawei/u8951/proprietary/lib/libpn544_fw.so:system/lib/libpn544_fw.so
